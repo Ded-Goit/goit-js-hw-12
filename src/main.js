35562a9c69b833b2353054d3151e56afcc236d24
Andriy Romanov <mgm.agro04@gmail.com>
2025-04-08 01:02:10 +0300

    for button load more

--- src/main.js
+++ src/main.js
@@ -89,3 +89,10 @@ function smoothScroll() {
     behavior: 'smooth',
   });
 }
+document.querySelector('.gallery-button').onmousemove = e => {
+  const x = e.pageX - e.target.offsetLeft;
+  const y = e.pageY - e.target.offsetTop;
+
+  e.target.style.setProperty('--x', `${x}px`);
+  e.target.style.setProperty('--y', `${y}px`);
+};
